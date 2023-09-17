b11dbd83ce04691b6e1572bc5437eda7cee2b878
QA3R <46014750+QA3R@users.noreply.github.com>
2023-09-17 19:15:07 -0400

    Added TurnTrackerUI.cs script to project

--- Assets/Scripts/UserInterface/TurnTrackerUI.cs
+++ Assets/Scripts/UserInterface/TurnTrackerUI.cs
@@ -10,17 +10,30 @@ namespace UserInterface
         private TextMeshProUGUI currentTurnTxt;
         private TextMeshProUGUI nextTurnTxt;
 
-
         // Start is called before the first frame update
         void Start()
         {
-        
+            //Subscribe to the EventHandlers OnStateEnd with UpdateTurnTracker
+
+            //Get access to the current unit
+            //Set the currentTurnTxt to the current unit name
+
+            //Get the unitIndex + 1
+            //Set the nextTurnTxt to the unitIndex +1
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnDisable()
         {
-        
+            //Unsubscribe to the EventHandler's OnStateEnd with UpdateTurnTracker
+        }
+
+        void UpdateTurnTracker()
+        {
+            //Get access to the current unit
+            //Set the currentTurnTxt to the current unit name
+
+            //Get the unitIndex + 1
+            //Set the nextTurnTxt to the unitIndex +1
         }
     }
 
